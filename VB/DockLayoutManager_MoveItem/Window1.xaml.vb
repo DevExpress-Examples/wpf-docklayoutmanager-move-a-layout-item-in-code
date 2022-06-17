45f5bacbaeec9fa6a4efb1dfa9799d1cac34b384
DevExpressExampleBot <fakeEmail>
2022-06-17 15:06:51 +0400

    Source auto update [skip ci]

--- VB/DockLayoutManager_MoveItem/Window1.xaml.vb
+++ VB/DockLayoutManager_MoveItem/Window1.xaml.vb
@@ -1,35 +1,23 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
 Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 Imports DevExpress.Xpf.Layout.Core
 
 Namespace DockLayoutManager_MoveItem
-	''' <summary>
-	''' Interaction logic for Window1.xaml
-	''' </summary>
-	Partial Public Class Window1
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+    ''' <summary>
+    ''' Interaction logic for Window1.xaml
+    ''' </summary>
+    Public Partial Class Window1
+        Inherits Window
 
-		Private Sub buttonMove_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
-'			#Region "#1"
-			dockLayoutManager1.LayoutController.Move(layoutItemEditor1, layoutItemButton1, MoveType.Left)
-'			#End Region ' #1
-		End Sub
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
 
-
-	End Class
+        Private Sub buttonMove_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+'#Region "#1"
+            Me.dockLayoutManager1.LayoutController.Move(Me.layoutItemEditor1, Me.layoutItemButton1, MoveType.Left)
+'#End Region  ' #1
+        End Sub
+    End Class
 End Namespace
