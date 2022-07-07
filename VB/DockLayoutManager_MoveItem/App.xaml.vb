280883f754b9d36e6e8440cab14ae5ca4c921060
DevExpressExampleBot <fakeEmail>
2022-07-07 13:18:28 +0400

    Source auto update [skip ci]

---
+++ VB/DockLayoutManager_MoveItem/App.xaml.vb
@@ -0,0 +1,12 @@
+Imports System.Windows
+
+Namespace DockLayoutManager_MoveItem
+
+    ''' <summary>
+    ''' Interaction logic for App.xaml
+    ''' </summary>
+    Public Partial Class App
+        Inherits Application
+
+    End Class
+End Namespace
